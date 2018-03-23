1903e600f5aa1182d6314b53c663059cb4802738
davidwroche <davidwroche@yahoo.co.uk>
2018-03-23 00:24:47 +0000

    base

---
+++ README.md
@@ -0,0 +1,15 @@
+<snippet>
+  <content>
+
+Vue js detect ad block component.
+
+## Installation
+
+
+## Usage
+
+TODO: Write usage instructions
+
+</content>
+  <tabTrigger>readme</tabTrigger>
+</snippet>
---
+++ adblock.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+<div id="wrapfabtest">
+    <div class="adBanner">
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+    data: function() {
+        return {
+        }
+    },
+    mounted: function(){
+
+        var wrap = document.getElementById("wrapfabtest").offsetHeight;
+        console.log(wrap)
+        if(wrap > 0) {
+        alert('No AdBlock :)');
+        
+         } else {
+        alert('AdBlock Detected ');
+        
+        }
+    }
+}
+</script>
+
+<style scoped>
+.adBanner {
+    background-color: transparent;
+    height: 1px;
+    width: 1px;
+}
+</style>
